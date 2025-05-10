c782fc617c5f651d1910c54661e891f21517ef5a
Hamedo2002 <106942077+Hamedo2002@users.noreply.github.com>
2025-05-10 06:37:40 +0300

    update

--- MamaPill-main/lib/features/medicine/data/models/medicine_model.dart
+++ MamaPill-main/lib/features/medicine/data/models/medicine_model.dart
@@ -2,6 +2,7 @@ import 'package:mama_pill/features/medicine/domain/entities/medicine.dart';
 
 class MedicineModel extends Medicine {
   const MedicineModel({
+    required super.id,
     required super.name,
     required super.type,
     required super.dose,
@@ -11,6 +12,7 @@ class MedicineModel extends Medicine {
   });
 
   factory MedicineModel.fromJson(Map<String, dynamic> json) => MedicineModel(
+        id: json["id"],
         name: json["name"],
         type: json["type"],
         dose: json["dose"],
